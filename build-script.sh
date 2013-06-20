e6b544be01da144fbb4148f15d71f1f251f808c6
Quinton Pike <qrpike@gmail.com>
2013-06-19 21:49:21 -0400

    Initial Commit

--- build-script.sh
+++ build-script.sh
@@ -2,4 +2,4 @@
 
 # Uglify the files:
 # requires: npm install uglify-js -g
-uglifyjs ./libs/watch.js ./libs/eventEmitter.js ./libs/requestAnimationFrame.js ./index.js -o ./minified/mdv.min.js --source-map ./minified/mdv.min.js.map
+uglifyjs ./libs/watch.js ./libs/eventEmitter.js ./libs/requestAnimationFrame.js ./index.js -o minified/mdv.min.js --source-map minified/mdv.min.js.map
